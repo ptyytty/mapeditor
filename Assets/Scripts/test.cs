cc75a5b65b9d80ffb84317949b35a043820a5d8a
typark <typark0916@gmail.com>
2025-03-16 14:27:08 +0900

    250316

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -13,11 +13,29 @@ public class test : MonoBehaviour
 
 	public bool ConnectionTest() {
 
-		string connection = "Server=localhost;Database=hello;userid=root;password=park;";
+		string connection = "Server=localhost;Database=job;userid=root;password=park;";
 
 		try {
 			using (MySqlConnection conn = new MySqlConnection(connection)) {
 				conn.Open();
+				string query = "Select * from ability";
+				using (MySqlCommand cmd = new MySqlCommand(query, conn)) {
+					using (MySqlDataReader reader = cmd.ExecuteReader()) {
+						while (reader.Read()) {
+							int id_job = reader.GetInt32("id_job");
+							string name = reader.GetString("id_name");
+							int hp = reader.GetInt32("hp");
+							int atk = reader.GetInt32("atk");
+							int def = reader.GetInt32("def");
+							int reg = reader.GetInt32("reg");
+							int mag = reader.GetInt32("mag");
+							int avoid = reader.GetInt32("avoid");
+							int hit = reader.GetInt32("hit");
+
+							Debug.Log($"id_code: {id_job}, Name: {name}, hp: {hp}, atk: {atk}, def: {def}, reg: {reg}, mag: {mag}, avoid: {avoid}, hit: {hit}");
+						}
+					}
+				}
 			}
 			return true;
 		}
